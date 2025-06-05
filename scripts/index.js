097964d7b2b0fabdd2de3a1b506d0242f6b11654
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-06-05 02:21:08 +0100

    Implemented the darkmode.

--- scripts/index.js
+++ scripts/index.js
@@ -35,6 +35,31 @@ window.addEventListener("scroll", () => {
     const nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+// DARKMODE JS
+
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+};
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+};
+
+if (darkmode === 'active') enableDarkmode();
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
+});
+
+
 // --------------------------------------------------------------------------
 
 
